30aa6ce8590ebcf47db9b6b538d451be5bf91afb
Begum Yılmaz <begum.software@gmail.com>
2022-10-03 04:27:18 +0300

    EntityFramework paketi eklendi. context sınıfı oluşturuldu.

---
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
+{
+    public class Context:DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Bill_Item> Bill_Items { get; set; }
+        public DbSet<Bills> Bills { get; set; }
+        public DbSet<Category> Categories{ get; set; }
+        public DbSet<Context> Contexts { get; set; }
+        public DbSet<Current> Currents{ get; set; }
+        public DbSet<Departman> Departmen { get; set; }
+        public DbSet<Employee> Employees{ get; set; }
+        public DbSet<Expense> Expenses{ get; set; }
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<SalesMovement> SalesMovements{ get; set; }
+
+        
+    }
+}
