22365eaecaaeb4ec273e83cf5ecc5dafbe6ba751
Begum Yılmaz <begum.software@gmail.com>
2022-10-05 04:17:54 +0300

    addition has been made.

--- MvcOnlineTicariOtomasyon/Controllers/CategoryController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CategoryController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Sınıflar;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+        Context c=new Context();    
         public ActionResult Index()
         {
+            
             return View();
         }
     }
