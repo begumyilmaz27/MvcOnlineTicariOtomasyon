fff98e5f42d9563505941f95b38d73e8c3191ee9
Begum Yılmaz <begum.software@gmail.com>
2022-10-09 01:31:25 +0300

    Newproduct and ProductBring viewları getirildi ve sayfa düzenlendi.

--- MvcOnlineTicariOtomasyon/Controllers/ProductController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/ProductController.cs
@@ -14,18 +14,60 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         Context c = new Context();
         public ActionResult Index()
         {
-            var products = c.Products.ToList();
+            var products = c.Products.Where(x=>x.ProductSituation==true).ToList();
             return View(products);
         }
         [HttpGet]
         public ActionResult NewProduct()
         {
+            List<SelectListItem> deger1 = (from x in c.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
             return View();
         }
         [HttpPost]
         public ActionResult NewProduct(Product p)
         {
             c.Products.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult ProductDelete(int id)
+        {
+            var deger = c.Products.Find(id);
+            deger.ProductSituation = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ProductBring(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+
+            var productdeger = c.Products.Find(id);
+            return View("ProductBring", productdeger);
+        }
+        public ActionResult ProductUpdate (Product p)
+        {
+            var urn = c.Products.Find(p.ProductID);
+            urn.ProductPurchasePrice=p.ProductPurchasePrice;
+            urn.ProductSituation=p.ProductSituation;
+            urn.CategoryId = p.CategoryId;
+            urn.ProductBrand = p.ProductBrand;
+            urn.ProductSalePrice = p.ProductSalePrice;
+            urn.ProductStock = p.ProductStock;
+            urn.ProductName=p.ProductName;
+            urn.ProductImage = p.ProductImage;
             c.SaveChanges();
             return RedirectToAction("Index");
         }
