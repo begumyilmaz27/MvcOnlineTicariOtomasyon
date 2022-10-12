6ba3ca2a509a4d5e0b6eadfe3881ce143af7adcc
Begum Yılmaz <begum.software@gmail.com>
2022-10-12 04:31:34 +0300

    Current Page adding.

---
+++ MvcOnlineTicariOtomasyon/Controllers/CurrentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Sınıflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CurrentController : Controller
+    {
+        // GET: Current
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Currents.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult NewCurrent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewCurrent(Current p)
+        {
+            c.Currents.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
